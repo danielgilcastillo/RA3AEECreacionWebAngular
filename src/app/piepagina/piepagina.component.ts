06b3c87f3b1361f1bdf571da3dfbe778971f38b5
danielgilcastillo <danielgil.23@campuscamara.es>
2024-11-22 17:59:51 +0100

    Add files via upload

---
+++ src/app/piepagina/piepagina.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-piepagina',
+  templateUrl: './piepagina.component.html',
+  styleUrls: ['./piepagina.component.sass']
+})
+export class PiepaginaComponent {
+  creador: string = 'Daniel Gil';
+  copyright: string = 'Copyright 2024'; 
+}
